c77c12a4cdfccc0149f20aad8bf1b6a0e522078f
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2022-01-23 22:54:03 +0300

    два блока задач

---
+++ seminar1/4.py
@@ -0,0 +1,52 @@
+# 4 Найти максимальное из трех чисел
+import random
+def GetRandom: random.randint(0,100)
+	return
+def PrintArray (array[]):
+    while i<3: print (A[i])
+
+
+for i in range (1,n+1):
+
+F=F*
+
+
+
+
+
+
+
+int i=0; //лучше делать через for  так не будет проблем с реопредлением i
+
+i=0;
+
+
+int max=0;
+int[] A = new int [3];
+
+while (i<3)
+
+{
+
+A[i]=GetRandom();
+
+i++;
+
+}
+
+i=0;
+max=A[i];
+
+while (i<3)
+
+{   if (A[i]>max) max=A[i];
+  i++;
+
+}
+
+
+Console.Write ("В массиве: ");
+i=0;
+PrintArray(A);
+Console.WriteLine();
+Console.WriteLine("Максимальное число "+ max);
